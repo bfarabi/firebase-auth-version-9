f3e855175b6b47001785b6a1e6935fb9e129aad8
Billal Farabi <bfarabi108@gmail.com>
2021-11-30 22:31:01 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,189 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { useState } from "react";
+
+initializeApp(firebaseConfig);
 
 function App() {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  const [newUser, setNewUser] = useState(false);
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    photo: "",
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+
+  const handleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((res) => {
+        const { displayName, email, photoURl } = res.user;
+        const signedInUser = {
+          isSignedIn: true,
+          photo: photoURl,
+          name: displayName,
+          email: email,
+        };
+        setUser(signedInUser);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        console.log(errorCode);
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        const email = error.email;
+      });
+  };
+  const handleSignOut = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        const signOutUser = {
+          isSignedIn: false,
+          photo: "",
+          name: "",
+          email: "",
+        };
+        setUser(signOutUser);
+      })
+      .catch((error) => {});
+  };
+  const handleSubmit = (e) => {
+    if (newUser && user.email && user.password) {
+      createUserWithEmailAndPassword(auth, user.email, user.password)
+        .then((userCredential) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = "";
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+          updateUserName(user.name);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = "Email already used";
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+        });
+    }
+    if (!newUser && user.email && user.password) {
+      signInWithEmailAndPassword(auth, user.email, user.password)
+        .then((res) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = "";
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = "Email already used";
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+        });
+    }
+
+    e.preventDefault();
+  };
+  const handleChange = (e) => {
+    let isFormValid = true;
+    if (e.target.name === "email") {
+      const isEmailValid = /\S+@\S+\.\S+/.test(e.target.value);
+      isFormValid = isEmailValid;
+    }
+    if (e.target.name === "password") {
+      const isPasswordValid =
+        e.target.value.length > 6 && e.target.value.length < 32;
+      const passwordHasNumber = /\d{1}/.test(e.target.value);
+      isFormValid = isPasswordValid && passwordHasNumber;
+    }
+    if (isFormValid) {
+      const newUserInfo = { ...user };
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo);
+    }
+  };
+  const updateUserName = (name) => {
+    updateProfile(auth.currentUser, {
+      displayName: name,
+    })
+      .then(() => {
+        console.log("name update successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      {user.isSignedIn ? (
+        <button onClick={handleSignOut}>Sign out</button>
+      ) : (
+        <button onClick={handleSignIn}>Sign in</button>
+      )}
+      {user.isSignedIn && (
+        <div>
+          <h1>Welcome {user.name} </h1>
+          <p> Your email: {user.email} </p>
+        </div>
+      )}
+      <h1>Our own authentication</h1>
+      <input
+        type="checkbox"
+        onChange={() => setNewUser(!newUser)}
+        name="newUser"
+      />
+      <label htmlFor="newUser">New user sign up</label>
+      <form action="" onSubmit={handleSubmit}>
+        {newUser && (
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            onBlur={handleChange}
+          />
+        )}
+        <br />
+        <input
+          type="email"
+          name="email"
+          id="input-email"
+          placeholder="Enter your email address"
+          onBlur={handleChange}
+          required
+        />
+
+        <br />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Enter your password"
+          onBlur={handleChange}
+          required
+        />
+        <br />
+        <input type="submit" value={newUser ? "Sign up" : "Sign in"} />
+      </form>
+      {user.success && (
+        <p style={{ color: "green" }}>
+          User account {newUser ? "created" : "logged in"} successfully
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
+      {user.error && <p style={{ color: "red" }}>{user.error}</p>}
     </div>
   );
 }
